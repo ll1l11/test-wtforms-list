5f8f459acd81ff7ab3b30d2f96026e5d70e6f961
ll1l11 <ll1l11@qq.com>
2016-05-19 17:18:23 +0800

    init

---
+++ README.rst
@@ -0,0 +1,10 @@
+Test Flask-WTF List
+===================
+
+如下的数据 
+
+    {'tag': ['a;, ‘b’]}
+
+FieldList兑现处理主要处理
+
+class TagForm(Form):
---
+++ example/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+
+
+
+
+
+def create_app(config=None):
+    app = Flask(__name__)
+
+
+    return app
